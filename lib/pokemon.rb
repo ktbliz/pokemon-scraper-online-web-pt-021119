3f3446fba7ec9b2c31280255f0d0bf63e75c3da9
Katie Blizzard <kblizzard11@gmail.com>
2019-04-30 04:33:46 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -6,11 +6,12 @@ class Pokemon
   
   @@all = []
   
-  def initialize(id:, name:, type:, db:) 
+  def initialize(id:, name:, type:, db:, hp: nil) 
     @id = id 
     @name = name 
     @type = type 
     @db = db 
+    @hp = hp
   end  
   
   def self.save(name, type, db) 
