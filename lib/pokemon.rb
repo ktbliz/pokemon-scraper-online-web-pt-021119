99201dd375af9639f9d6f5a6e6e967af68281526
Katie Blizzard <kblizzard11@gmail.com>
2019-04-30 03:46:06 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
+#require 'pry'
+
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db 
+  
+  @@all = []
+  
+  def initialize(id: , name: , type: , db: ) 
+    @id = id 
+    @name = name 
+    @type = type 
+    @db = db 
+  end  
+  
+  def self.save(name, type, db) 
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end 
+  
+  def self.find(id, db) 
+    array = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+    new_pokemon = self.new(id: array[0] , name: array[1] , type: array[2] , db: db)
+    #binding.pry
+  end 
+  
 end
+  
